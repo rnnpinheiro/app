b5daf3b8223f524471fddc8aa5347da7298f24a0
Renan <rnn.pinheiro@gmail.com>
2024-09-28 15:57:20 -0300

    adiciona inquirer

---
+++ conditional.js
@@ -0,0 +1,20 @@
+// condicionais, switch
+
+const start = () => {
+
+    while(true) {
+        let opcao = "cadastrar"
+        switch(opcao) {
+            case "cadastrar":
+                console.log("Vamos cadastrar");
+                break
+            case "listar":
+                console.log("Vamos listar");
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start();
